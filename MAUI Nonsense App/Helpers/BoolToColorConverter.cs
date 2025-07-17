9886eee307fbb6392de929433d252ef26a7277b9
pudelosha <pudel1985@gmail.com>
2025-07-17 23:32:27 +0200

    Fix to LightPage v2

---
+++ MAUI Nonsense App/Helpers/BoolToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace MAUI_Nonsense_App.Helpers;
+
+public class BoolToColorConverter : IValueConverter
+{
+    public Color TrueColor { get; set; } = Colors.LightGreen;
+    public Color FalseColor { get; set; } = Colors.Transparent;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+            return b ? TrueColor : FalseColor;
+
+        return FalseColor;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
